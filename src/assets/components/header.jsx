39204cbcc0debee8ef71acd8bcac1f48b1405df9
ALMA_DEV914 <aungonalna58@gamil.com>
2026-01-04 21:35:32 -0800

    add winning feature componnt

--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -1,11 +1,17 @@
 export const Header = ({ score, moves }) => {
-  return (<div className="game-header"> <h1>Memory Card Game</h1>
-  <div className="stat">
-  <div className="stat-item"><span className="label">Score: </span>{" "}<span className="value">{score}</span></div>
-  <div className="stat-item"></div>
-  <div className="stat-item"><span className="label">Moves:</span>{" "}<span className="value">{moves}</span>
-  </div>
-  </div>
-  </div>
+  return (
+    <header className="game-header">
+      <h1 className="game-title">Memory Card Game</h1>
+      <div className="stats">
+        <div className="stat-item">
+          <span className="label">Score</span>
+          <span className="value">{score}</span>
+        </div>
+        <div className="stat-item">
+          <span className="label">Moves</span>
+          <span className="value">{moves}</span>
+        </div>
+      </div>
+    </header>
   );
-}
+};
